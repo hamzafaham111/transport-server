d563d0f23fd3cf4e12527b73b787fd32c9fc31af
hamzafaham111 <hamzafaham111@gmail.com>
2023-01-12 03:53:33 +0500

    dd

--- Models/ProductsSchema.js
+++ Models/ProductsSchema.js
@@ -2,11 +2,9 @@ const mongoose = require('mongoose');
 const ProductSchema = mongoose.Schema({
     productDescription: {
         type: String,
-        required: true,
     },
     netPrice: {
         type: String,
-        required: true,
     },
     pricePerKg: {
         type: String,
